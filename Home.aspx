233f2ad83584bfa42cfb6f77207db3019c757993
Monisha Ishwar <monishamoger15@gmail.com>
2025-09-24 18:05:35 +0530

    Add project files.

---
+++ Home.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Web_Configartion_Practice.Home" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Home Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter Marks: 
+            <asp:TextBox ID="txtMarks" runat="server"></asp:TextBox>
+            <asp:Button ID="btnCheck" runat="server" Text="Check Pass/Fail" OnClick="btnCheck_Click" />
+            <br /><br />
+            <asp:Label ID="lblResult" runat="server" ForeColor="Blue"></asp:Label>
+            <hr />
+            <asp:Button ID="btnShow" runat="server" Text="Show Products" OnClick="btnShow_Click" />
+            <br /><br />
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Home.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_Configartion_Practice
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Do nothing on first load
+        }
+
+        protected void btnShow_Click(object sender, EventArgs e)
+        {
+            string conStr = ConfigurationManager.ConnectionStrings["ShopDB"].ToString();
+            SqlConnection con = new SqlConnection(conStr);
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("SELECT TOP 5 Cid, Name, Email FROM Customers", con);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void btnCheck_Click(object sender, EventArgs e)
+        {
+            int passMarks = Convert.ToInt32(ConfigurationManager.AppSettings["passmarks"]);
+            int marks = Convert.ToInt32(txtMarks.Text.Trim());
+
+            if (marks >= passMarks)
+            {
+                lblResult.Text = "Result: Pass 😊";
+                lblResult.ForeColor = System.Drawing.Color.Green;
+            }
+            else
+            {
+                lblResult.Text = "Result: Fail ☹️";
+                lblResult.ForeColor = System.Drawing.Color.Red;
+            }
+
+        }
+    }
+}
